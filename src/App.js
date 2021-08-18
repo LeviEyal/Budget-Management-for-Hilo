161dbb46679c3be73d73075451006622cc7bb307
LeviEyal <happyeyal@gmail.com>
2021-08-18 10:09:43 +0300

    designing the app fonts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import CategoryNav from "./components/CategoryNav/CategoryNav";
+import EditFields from "./components/EditFields/EditFields";
+import FieldForm from "./components/FieldForm/FieldForm";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <CategoryNav />
+      <EditFields />
+      <FieldForm />
     </div>
   );
 }
-
-export default App;
