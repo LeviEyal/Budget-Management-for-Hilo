afd9cd13beb8c192ebc2baaf2b9a3582204d4ef8
LeviEyal <happyeyal@gmail.com>
2021-08-18 10:08:37 +0300

    designing the EditFields component

---
+++ src/components/FieldForm/FieldForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./FieldForm.css";
+
+export default function FieldForm() {
+  return (
+    <div className="field-form">
+      <form>
+        <p>Description</p>
+        <input type="text" />
+        <p>Date</p>
+        <input type="text" />
+        <p>Amount of Money</p>
+        <input type="text" />
+        <p>Note</p>
+        <input type="text" />
+        <br /><br /><br />
+        <br /><br /><br />
+        <button className="add-btn">+ Add</button>
+        <br />
+        <button className="submit-btn">Submit</button>
+      </form>
+    </div>
+  );
+}
