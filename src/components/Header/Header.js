a1cbbf697f73c2f8ee00b6dde0485da662745e24
LeviEyal <happyeyal@gmail.com>
2021-08-18 10:08:10 +0300

    designing the header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+   return (
+      <header className="app-header">
+         <h1>Build your Budget</h1>
+         <h1>____</h1>
+         <br/>
+         <h4 className="slogen">
+         Why not leveraging technology to save your time and allow you to utilize it for  focusing on what’s really matter?
+         </h4>
+      </header>
+   )
+}
