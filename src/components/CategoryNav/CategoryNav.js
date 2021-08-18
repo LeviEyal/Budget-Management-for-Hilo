38ed57570a3a5525883238504f589289a2ec396f
LeviEyal <happyeyal@gmail.com>
2021-08-18 10:09:08 +0300

    designing the CategoryNav component

---
+++ src/components/CategoryNav/CategoryNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CategoryNav.css'
+
+export default function CategoryNav() {
+   return (
+      <div className="navButton">
+         <div className="t">
+            <button>Income</button>
+            <button>Expenses</button>
+         </div>
+      </div>
+   )
+}
