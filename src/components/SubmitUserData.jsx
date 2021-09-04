52657b725ac92a2198772b4cf8b7db8d8ba20b82
LeviEyal <happyeyal@gmail.com>
2021-09-04 16:53:17 +0300

    write user data to firebase

--- src/components/SubmitUserData.jsx
+++ src/components/SubmitUserData.jsx
@@ -1,8 +1,11 @@
+import firebase from "../Firebase";
 
 export default function SubmitUserData({userData}) {
 
   function writeUserDataToFirebase(params) {
-    
+    const dbRef = firebase.database().ref();
+    dbRef.child('users/user1')
+        .set(userData);
   }
 
   return (
